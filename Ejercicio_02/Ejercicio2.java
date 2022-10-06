6aa218a104e65c179fb6e28071eb496634df2bf5
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-06 09:03:37 +0200

    Ejercicios del capitulo 1

---
+++ Ejercicio_02/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+    System.out.println("Antonio Luis Garcia Guerrero");
+    System.out.println("Calle Vista Hermosa Nº18");
+    System.out.println("685236841");
+    }
+    }
---
+++ Ejercicio_03/Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+    System.out.println("Computer     Ordenador");
+    System.out.println("Cat          Gato");
+    System.out.println("Dog          Perro");
+    System.out.println("book         libro");
+    System.out.println("light        luz");
+    System.out.println("green        verde");
+    System.out.println("pyramid      piramide");
+    System.out.println("nature       naturaleza");
+    System.out.println("machine      maquina");
+    System.out.println("student      estudiante");
+    }
+    }
