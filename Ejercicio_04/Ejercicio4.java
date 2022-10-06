01729d18761229cb74c3c68e11ffbb62cba20ebb
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-06 10:14:40 +0200

    modificacion de los ejercicios

--- Ejercicio_04/Ejercicio4.java
+++ Ejercicio_04/Ejercicio4.java
@@ -1,12 +1,23 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
-    System.out.println("Lunes           Martes         Miercoles      Jueves         Viernes");
-    System.out.println("-----------------------------------------");
-    System.out.printf("%-15s    %-12s   %-15s   %-15s   %-15s","Programacion","Sistemas","Entornos","Lenguaje","FOL");
-    System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-    System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-    System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-    System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-    System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
+        System.out.println("Lunes           Martes         Miercoles      Jueves         Viernes");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","Programacion","Sistemas","Entornos","Programacio","FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","Programacion","Sistemas","Entornos","Programacion","FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","Lenguajes","Sistemas","Programacion","Lenguajes","FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","Lenguajes","Programacion","Programacion","Lenguajes","Sistemas");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","BD","Programacion","BD","BD","Sistemas");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","BD","Entornos","BD","BD","Sistemas");
+
     }
     }
--- Ejercicio_05/Ejercicio5.java
+++ Ejercicio_05/Ejercicio5.java
@@ -1,12 +1,23 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
         System.out.println("Lunes           Martes         Miercoles      Jueves         Viernes");
-        System.out.println("-----------------------------------------");
-        System.out.printf("%-15s    %-15s   %-15s   %-15s   %-15s","Programacion","Sistemas","Entornos","Lenguaje","FOL");
-        System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-        System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-        System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-        System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
-        System.out.printf("%-10s    %-10s   %-10s   %-10s   %-10s","");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[32m Programacion","\033[34m Sistemas","\033[36m Entornos","\033[32m Programacion","\033[35m FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[32m Programacion","\033[34m Sistemas","\033[36m Entornos","\033[32m Programacion","\033[35m FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[33m Lenguajes","\033[34m Sistemas","\033[32m Programacion","\033[33m Lenguajes","\033[35m FOL");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[33m Lenguajes","\033[32m Programacion","\033[32m Programacion","\033[33m Lenguajes","\033[34m Sistemas");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[31m BD","\033[32m Programacion","\033[31m BD","\033[31m BD","\033[34m Sistemas");
+        System.out.println("");
+
+        System.out.printf("%-12s    %-12s   %-12s   %-12s   %-15s","\033[31m BD","\033[36m Entornos","\033[31m BD","\033[31m BD","\033[34m Sistemas");
+
     }
     }
