890443c7f73a908c03456107635874137e1a238b
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-10-10 10:26:07 +0200

    cambio los caracteres del 9 y 10

---
+++ Ejercicio_10/Ejercicio_10.java
@@ -0,0 +1,17 @@
+public class Ejercicio_10 {
+    public static void main(String[] args) {
+    
+        String rojo = "\033[32m";
+        
+    System.out.println(rojo);
+    System.out.println("       ▄                           Bienvenidos          ");
+    System.out.println("     ▄ ▄ ▄         ▄ ▄ ▄ ▄         a          ");
+    System.out.println("   ▄ ▄ ▄ ▄ ▄        ▄ ▄ ▄          mi          ");
+    System.out.println("▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄        humilde          ");
+    System.out.println("▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄        morada,          ");
+    System.out.println("▄ ▄ ▄__|__▄ ▄ ▄ ▄ ▄     ▄ ▄        disfrutar          ");
+    System.out.println("▄ ▄ ▄  |  ▄ ▄ ▄ ▄ ▄  .  ▄ ▄        la          ");
+    System.out.println("▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄     ▄ ▄        estancia         ");
+    }
+    }
+   
